9178a6bfa86071d6f4692fd9bb11867ba8fd51c4
chentong <2778415631@qq.com>
2021-08-04 16:10:39 +0800

    更新左右过度效果

--- src/App.vue
+++ src/App.vue
@@ -13,19 +13,55 @@ export default {
   name: "app",
   data() {
     return {
-      transitionName: "slide-left"
+      transitionName: "slide-left",
+      // include: isKeepAlive(this.$route) ? [this.$route.name] : [],
+      history: []
     };
   },
   watch: {
     $route(to, from) {
-      const prevStamps = from.query.stamps || 0;
-      const nextStamps = to.query.stamps || 0;
-      this.transitionName = prevStamps > nextStamps ? "slide-right" : "slide-left";
-		console.log(from.query.stamps);
-		console.log(to.query.stamps);
+       const lastPath = this.history[this.history.length - 1] || {},
+        { isReplace, isBack } = this.$router;
+     if (lastPath.path === to.path) {
+        this.transitionName = "slide-right";
+        this.history.pop();
+      } else {
+        this.transitionName = "slide-left";
+        if (!isReplace) this.history.push({ path: from.path, name: from.name });
+      }
     }
 	// if()
   }
+  //   watch: {
+  //   $route(to, from) {
+  //     const lastPath = this.history[this.history.length - 1] || {},
+  //       { isReplace, isBack } = this.$router;
+  //       // console.log(isReplace,isBack);
+  //       console.log( this.history);
+
+  //     if (lastPath.path === to.path) {
+  //       this.transitionName = "fold-right";
+  //       this.history.pop();
+  //     } else {
+  //       this.transitionName = "fold-left";
+  //       if (!isReplace) this.history.push({ path: from.path, name: from.name });
+  //     }
+
+  //     if (isKeepAlive(to) && to.name !== "Login") {
+  //       !this.include.includes(to.name) && this.include.push(to.name);
+  //     }
+
+  //     if (isKeepAlive(from) && isBack) {
+  //       var index = this.include.indexOf(from.name);
+  //       index !== -1 && this.include.splice(index, 1);
+  //     }
+
+  //     this.$router.isBack = false;
+  //     this.$router.isReplace = false;
+
+  //     console.log(this.transitionName, "change");
+  //   }
+  // },
 };
 </script>
 <style>
@@ -33,33 +69,31 @@ export default {
   position: absolute;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   box-sizing: border-box;
   transition-property: transform,opacity;
   transition-duration: 0.3s;
   transition-timing-function: ease-out;
 }
 .slide-left-enter {
-  opacity: 0;
+  opacity: 1;
   transform: translate(100%, 0);
 }
-.slide-left-enter-active {
-  z-index: 10;
-}
+
 .slide-left-leave-active {
   z-index: 0;
+   opacity: 0;
 }
-.slide-right-leave-active {
-  opacity: 0;
-  transform: translate(100%, 0);
+.slide-right-enter {
+  opacity: 1;
+  transform: translate(-100%, 0);
   /* z-index: 11; */
 }
-.slide-right-enter-active {
-  z-index: 10;
-}
+
 .slide-right-leave-active {
   z-index: 0;
+   opacity: 0;
 }
 </style>
 
